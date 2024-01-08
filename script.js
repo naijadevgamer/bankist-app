b092c659f5435e49f1519595cd21195018a1ea2c
thenaijagamer <sabdullahialaba50@gmail.com>
2024-01-08 18:35:43 +0100

    Add more functionalities

--- script.js
+++ script.js
@@ -78,6 +78,21 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsername(accounts);
+
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
